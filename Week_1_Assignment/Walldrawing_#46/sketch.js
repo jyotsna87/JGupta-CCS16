4bcef6eb5e8ea343c94f95aff114f50279a47495
Jyotsna Gupta0 <jg4570@nyu.edu>
2016-02-01 20:30:31 -0500

    replaced with variables
    made some changes

--- Week_1_Assignment/Walldrawing_#46/sketch.js
+++ Week_1_Assignment/Walldrawing_#46/sketch.js
@@ -29,30 +29,8 @@ function drawShape()
     line(start+i+tLength,yt+tLength, start+i,yt+2*tLength)
     line(start+i,yt+2*tLength, start+i,y2)
   }
-  //line(50+0,0, 50+0,yt+5)
+ 
 }
 function draw() {
-/*
-  stroke(124,252,0);
-  strokeWeight(3)
-  var x1=0
-  var y1=0
-  var xt=0
-  var yt=0
-  var x2=0
-  var y2=800
-  
-  yt=random(y2)
-  print(yt)
-    line(50+0,0, 50+0,yt)
-    line(50+0+5,0, 50+0,yt+5)
-    line(50+0,0, 50+0,yt+5)
-    */
-    /*
-  for(i=0;i<500*10;i=i+10)
-  {
-    yt=random(y1,y2)
-    line(0+i,0, 0+i,yt)
-  }
-  */
+
 }
